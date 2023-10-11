dc4464d97d7e39a6f4f25dbf673d68ee8d4665ab
mannu chaurasiya <mannuchaurasiya36@gmail.com>
2023-10-11 23:30:31 +0530

    first commit

---
+++ Digital_clock/script.js
@@ -0,0 +1,8 @@
+const clock = document.getElementById('clock');
+
+setInterval(function(){
+let date = new Date();
+clock.innerHTML = date.toLocaleTimeString();
+
+
+},1000)
---
+++ color_change/script.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body');
+
+buttons.forEach(function(button){
+ console.log(button);
+ button.addEventListener('click', function (e){
+  console.log(e);
+  console.log(e.target);
+  if(e.target.id ==='grey'){
+    // body.style.background = "gray"
+   // or
+   body.style.backgroundColor = e.target.id;
+
+  }
+  if(e.target.id === 'white'){
+   body.style.backgroundColor=e.target.id;
+
+  }
+  if(e.target.id === 'blue'){
+   body.style.backgroundColor = e.target.id;
+  }
+  if(e.target.id === 'yellow'){
+   body.style.backgroundColor = e.target.id;
+  }
+ });
+});
